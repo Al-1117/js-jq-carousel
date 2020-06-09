300390c365f914512b4eac654eeee2d7c13375e7
Alioune <alioune1117@gmail.com>
2020-06-09 15:54:30 +0200

    completata la funzionalità per dello slider, mancano i cerchietti

--- main.js
+++ main.js
@@ -1,38 +1,70 @@
-// Creo l'evento del click sulla freccia dello slider
 
-var next = $('.slider-wrapper .next')
-var previous = $('.slider-wrapper .prev')
+// CREO LA FUNZIONALITA' DEL CLICK PER ANIMARE LO SLIDER
+
 
 $(document).ready(
-  $(next).click(
-    function (){
-      scorreriAlProssimo();
-    }
+  function() {
+    // per scorrere al prossimo utilizzzando la funzione creata
+    var next = $('.slider-wrapper .next');
 
-  )
+    $(next).click(
+      function (){
+        scorreriProssimo();
+      }
+    );
 
+    // per scorrere al precedente utilizzando la funzione creata
+    var previous = $('.slider-wrapper .prev');
+
+    $(previous).click(
+      function(){
+        scorriPrecedente();
+      }
+    );
+
+  }
 );
 
 
-// Funzione per scorre alla prossima immagine
+// FUNZIONE PER SCORRERE AL PROSSIMO
 
-function scorreriAlProssimo(){
+function scorreriProssimo(){
   // Creo le variabili per le immagini
   var immagineCorrente = $('.slider-wrapper .images .active');
   var immaginePrima = $('.images .first');
-  
+
   // Gli tolgo la classe active
   $(immagineCorrente).removeClass('active');
 
-  // se l'immagine corrente è lultimo, gli do la classe active per farlo tornare alla prima pagina
+  // se l'immagine corrente è lultimo, gli do la classe active per farlo tornare alla prima immagine
   if (immagineCorrente.hasClass('last')) {
     $(immaginePrima).addClass('active');
 
+    // altrimenti aggiungo la classe active all'immagine successiva facendo scorrere lo slider
   } else {
     immagineCorrente.next('img').addClass('active');
   }
 
+}
 
+// FUNZIONE PER SCORRERE AL PRECEDENTE
+
+function scorriPrecedente(){
+  // Creo le variabili per le immagini
+  var immagineCorrente = $('.slider-wrapper .images .active');
+  var immagineUltima = $('.images .last');
+
+  // Gli tolgo la classe active per poter visualizzare il precedente
+  $(immagineCorrente).removeClass('active');
+
+  // se l'immagine corrente è il primo, gli do la classe active per farlo tornare all'ultima immagine
+  if (immagineCorrente.hasClass('first')) {
+    $(immagineUltima).addClass('active');
+
+    // altrimenti aggiungo la classe active all'immagine successiva facendo scorrere lo slider
+  } else {
+    immagineCorrente.prev('img').addClass('active');
+  }
 
 
 }
